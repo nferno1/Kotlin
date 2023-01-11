f9559c5e272e49aa7d39cb1e122aafb73b83bfab
Roman Podkopaev <nferno1@mail.ru>
2023-01-11 22:36:43 +0700

    Kotlin when, for

---
+++ src/main/kotlin/Module_05/main.kt
@@ -0,0 +1,33 @@
+package Module_05
+
+fun main(){
+
+    /*println("Enter a number") for
+    val n = readLine()?.toIntOrNull() ?: return
+
+    var sum = 0
+    for (j in 0..n){ // переменная j объявлена внутри цикла, к ней можно обратиться только внутри тела цикла
+        if (j % 2 != 0) continue // выведет только четные числа
+        println(j)
+        sum += j
+
+
+    }
+   println("Sum of first $n - $sum")*/
+
+
+    /*println("Enter a number") // while
+    val n = readLine()?.toIntOrNull() ?: return
+
+    var i: Int = 0
+    var sum = 0
+
+    while (i <= n) { // цикл будет выполнятся пока i <= n
+        println(i)
+        sum += i
+        i++ // увличиваем в конце цикла так как i не изменяеться м она всегда < n
+    }
+   println("Sum of first $n - $sum")*/
+
+
+}
